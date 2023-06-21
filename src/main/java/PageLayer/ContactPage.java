98632057952526a1650345072f39f4094ee63844
sanjayautomation <treestechnospace@gmail.com>
2023-06-21 10:16:43 +0530

    ContactPage

---
+++ src/main/java/PageLayer/ContactPage.java
@@ -0,0 +1,10 @@
+package PageLayer;
+
+public class ContactPage {
+
+	public static void main(String[] args) {
+		System.out.println("ContactPage Class");
+
+	}
+
+}
---
+++ src/test/java/TestLayer/ContactPageClass.java
@@ -0,0 +1,10 @@
+package TestLayer;
+
+public class ContactPageClass {
+
+	public static void main(String[] args) {
+		System.out.println("ContactPageTest");
+
+	}
+
+}
